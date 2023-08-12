6555f0fcbce4a3299c0e9dd89c5b47f205e0cb02
AlexUA_ubuntu <alex2011ua@gmail.com>
2023-08-12 23:23:43 +0000

    O

--- OnlineStoreDjango/my_apps/shop/api_v1/paginators.py
+++ OnlineStoreDjango/my_apps/shop/api_v1/paginators.py
@@ -3,17 +3,17 @@ from rest_framework.pagination import PageNumberPagination
 
 class LargeResultsSetPagination(PageNumberPagination):
     page_size = 100
-    page_size_query_param = 'page_size'
+    page_size_query_param = "page_size"
     max_page_size = 1000
 
 
 class StandardResultsSetPagination(PageNumberPagination):
     page_size = 10
-    page_size_query_param = 'page_size'
+    page_size_query_param = "page_size"
     max_page_size = 100
 
 
 class SmallResultsSetPagination(PageNumberPagination):
     page_size = 4
-    page_size_query_param = 'page_size'
+    page_size_query_param = "page_size"
     max_page_size = 100
